ec075dfdce95435883aa10edecf28d1a0585e2f6
JxanPadilla <juanlm101120@gmail.com>
2022-11-27 17:22:23 -0500

    listoo

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 import express from "express";
-import dotenv from "dotenv";
-import conectarDB from "./config/db.js";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import conectarDB from './config/db.js';
+import insumosRoutes from './routes/insumosRoutes.js';
 
 const PORT = process.env.PORT || 4000;
-
-const app = express();
 dotenv.config();
+
+// Se le agrega toda la funcionalidad del servidor de express
+const app = express();
+app.use(express.json());
+
 conectarDB();
 
-// Ruta de ejemplo
-// app.use('/api/usuarios', usuarioRoutes);
+// middlewares
+// Se utiliza para realizar la comunicacion entre el servidor del frontend y el backend
+// const dominiosPermitidos = [process.env.FRONTEND_URL];
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (dominiosPermitidos.indexOf(origin) !== -1) {
+//             // El origen del Request esta permitido
+//             callback(null, true);
+//         } else {
+//             callback(new Error('No permitido por CORS'));
+//         }
+//     }
+// };
+
+// app.use(cors(corsOptions));
+
+//gestion insumos
+app.use('/api/insumos', insumosRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto ${PORT} `);
