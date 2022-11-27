52a9d18c81ed4d455cd97aedbeae559f7256369e
JxanPadilla <juanlm101120@gmail.com>
2022-11-27 17:12:02 -0500

    solucionao

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 dotenv.config();
+conectarDB();
 
 // Ruta de ejemplo
 // app.use('/api/usuarios', usuarioRoutes);
